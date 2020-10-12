8bd8443593e03947cf26f3b12d78da24b1614819
busybox11 <planetgeek11@gmail.com>
2020-10-12 09:49:15 +0200

    Add tab support and prevent empty username

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 usernameSpan = document.getElementById('nav-panel-bottom-bar-username')
 
+chatBtn = document.getElementById('nav-btn-panel-chat')
+serversBtn = document.getElementById('nav-btn-panel-servers')
+
 function askUsername() {
     username = window.prompt('Enter a username', 'anon');
 
-    if (username == null) {
+    if (username == null || username == "") {
         askUsername()
     } else {
         usernameSpan.innerHTML = username
     }
 }
 
+function setPanelTab(tab) {
+    document.getElementsByClassName('nav-icon-active')[0].classList.remove('nav-icon-active')
+    document.getElementById(`nav-btn-panel-${tab}`).children[0].classList.add('nav-icon-active')
+}
+
 askUsername()
 
-usernameSpan.onclick = function() { askUsername() } 
+usernameSpan.onclick = function() { askUsername() }
+
+chatBtn.onclick = function() { setPanelTab('chat') }
+serversBtn.onclick = function() { setPanelTab('servers') }
